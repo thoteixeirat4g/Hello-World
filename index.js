f990c786e60ab6f7da7486d0bef909dbb0d38649
thoteixeirat4g <thomas@t4g.com.br>
2025-05-06 16:16:43 +0000

    railway

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
